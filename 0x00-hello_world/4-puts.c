542a68ad4552f077ffcb514a8b477c83091f0d07
brianmanoti <brianmanoti254@gmail.com>
2022-12-08 15:26:21 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,7 +4,7 @@
  * main - prints the phrase "Programming is like building a multilingual puzzle
  * return: 0 if exitted properly
  */
-int main (void)
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);

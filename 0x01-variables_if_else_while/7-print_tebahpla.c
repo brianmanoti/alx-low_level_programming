cd531c1a2c2d5d5ec162351c5a3148919d13d082
brianmanoti <brianmanoti254@gmail.com>
2022-12-10 11:54:12 -0800

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - printss all the numbers of base 16 in lower case
+ * followed by a new line
+ * Return: Always 0
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'z'; ch >= 'a'; ch--)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}

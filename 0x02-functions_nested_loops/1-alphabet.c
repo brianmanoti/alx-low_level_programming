2fe666dbab0c5e2b6cffe432e6088d9c35e596cd
brianmanoti <brianmanoti254@gmail.com>
2022-12-14 06:04:45 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - prints the alphabet in lower case
+ * followed by a newline
+ * Return: 0 Always
+ */
+void print_alphabet(void)
+{
+	char j;
+	for (j = 'a'; j <= 'z'; j++)
+	{
+		_putchar(j);
+	}
+	_putchar('\n');
+	return (0);
+}

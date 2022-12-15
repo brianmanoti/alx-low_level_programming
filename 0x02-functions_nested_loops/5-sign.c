5307c28ceaf158deec779d1a68161621051d0beb
brianmanoti <brianmanoti254@gmail.com>
2022-12-15 08:52:21 -0800

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,19 +8,17 @@
  */
 int print_sign(int n)
 {
-	int n;
-
 	if (n > 0)
 	{
-		_putchar('+');
-		Return (1);
-	}else if (n == 0)
-	{
-		_putchar(48);
-		return (0);
-	}else if (n < 0)
-	{
-		_putchar('-');
+		if (n == 0)
+			_putchar('0');
+			return (0);
+		{
+			if (n < 0)
+				_putchar('-');
+				return (-1);
+		}
 	}
-	return (-1);
+	_putchar('1');
+	return (1);
 }

7db9574a2acbbb3cba1b1518581063da53e2c8fb
brianmanoti <brianmanoti254@gmail.com>
2023-01-09 05:13:08 -0800

    static libraries

---
+++ 0x09-static_libraries/0-memset.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _memset - fills memeory with a constant byte
+ * @s: memory to be filled
+ * @b: char to copy
+ * @n: number of times to copy b
+ *
+ * Return: pointer to the memeory area s
+ */
+char *_memset(char *s, char b, unsigned int n);
+{
+

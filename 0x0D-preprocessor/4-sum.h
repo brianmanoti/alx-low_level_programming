e32087a658bbfe62e0a6f5988a2cf5c49b4148c7
brianmanoti <brianmanoti254@gmail.com>
2023-01-17 04:04:44 -0800

    4-sum.h

--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -2,12 +2,11 @@
 #define SUM_H
 
 /**
+ * File: 4-sum.h
  * @x: integer 1
  * @y: integer 2
  * Desc: header file that defines a macro named
  * SUM(x, y) that computes the sum of x and y.
  */
-
 #define SUM(x, y) ((x) + (y))
-
 #endif
